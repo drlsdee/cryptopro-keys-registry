9a03e4f93cbc31040ffeac61b0a03335d519d530
drlsdee <tracert0@gmail.com>
2018-11-30 16:15:45 +0500

    fixed registry path

--- script00.ps1
+++ script00.ps1
@@ -76,10 +76,10 @@ $UserSIDs = Get-ADGroup -LDAPFilter "(Name=$GroupName)" | Get-ADGroupMember | Wh
 
 IF ((Get-WmiObject Win32_OperatingSystem -ComputerName $PC).OSArchitecture -like "64*") {
     # Path to regkeys on x64
-    $RegRoot = "HKLM:SOFTWARE\Wow6432Node\Crypto Pro\Settings\Users\"
+    $RegRoot = "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Crypto Pro\Settings\Users\"
     } ELSE {
         # and x86
-        $RegRoot = "HKLM:SOFTWARE\Crypto Pro\Settings\Users\"
+        $RegRoot = "HKEY_LOCAL_MACHINE\SOFTWARE\Crypto Pro\Settings\Users\"
         }
 
 
@@ -90,7 +90,7 @@ $endOfPath = "\Keys"
 $KeyHive = $RegRoot + $SID + $endOfPath
 
 # Select hives with keys
-$Keys = Get-ChildItem $KeyHive |  Where-Object Name -Like $GroupName
+$Keys = Get-ChildItem Registry::$KeyHive |  Where-Object Name -Like $GroupName
 
 $UserSIDs
 $PCnames

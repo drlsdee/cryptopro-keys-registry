d8127197a131ef59fde88d86d38b20878e5dab55
drlsdee <tracert0@gmail.com>
2019-10-10 20:50:43 +0500

    Added functions
    Module 'Filter-CertificateSubject.psm1'
    Functions to generate cryptocontainer name
    Todo: Filter OIDs

--- Functions/Filter-CertificateSubject.psm1
+++ Functions/Filter-CertificateSubject.psm1
@@ -115,3 +115,116 @@ function Filter-CertificateSubject {
     }
 }
 Export-ModuleMember -Function "Filter-CertificateSubject"
+
+function ConvertFrom-QuotedStringToArray {
+    param (
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true)]
+        # Input string with quotes
+        [string]
+        $InputString,
+
+        [Parameter()]
+        # Quote char
+        [char]
+        $QuoteChar = '"'
+    )
+    [array]$unQuotedArray = $InputString -split $QuoteChar | Where-Object -FilterScript {$_.Length -gt 0} | ForEach-Object {$_.Trim(' ')}
+    return $unQuotedArray
+}
+
+function New-Abbreviation {
+    param (
+        [Parameter(Mandatory=$true)]
+        # Input array of strings
+        [string[]]
+        $StringArray,
+
+        [Parameter()]
+        # Max letters count per word (if some words will be shorter, these words will not be shortened).
+        [int]
+        $MaxWordLength = 3,
+
+        [Parameter()]
+        # Character for joining words
+        [char]
+        $JoinChar #= ''
+    )
+    $outArray = @()
+    foreach ($word in $StringArray) {
+        $subStringLength = $MaxWordLength
+        if ($word.Length -lt $MaxWordLength) {
+            $subStringLength = $word.Length
+        }
+        $wordShortened = $word.Substring(0,$subStringLength)
+        $outArray += $wordShortened
+    }
+    [string]$outString = $outArray -join $JoinChar
+    return $outString
+}
+
+function ConvertTo-PSObjectFromCertificateSubject {
+    param (
+        [Parameter(Mandatory=$true)]
+        # Subject or issuer as array of strings, each contains both field name and value, i.e. "CN=Common Subject Name"
+        [string[]]
+        $Subject,
+
+        [Parameter()]
+        # Field separator, default is '='
+        [char]
+        $FieldSeparator = '='
+    )
+    $OutputObject = New-Object -TypeName psobject
+    foreach ($string in $Subject) {
+        $stringSeparated = $string -split $FieldSeparator
+        $OutputObject | Add-Member -MemberType NoteProperty -Name $stringSeparated[0] -Value $stringSeparated[1]
+    }
+    return $OutputObject
+}
+
+<#
+Date in format 'yyyy-MM-dd'
+OrgSubject - 'O' as abbreviation
+OrgSubject - 'CN' as 'Surname_Name_Patronym'
+Roles from OIDs and .Extensions.EnhancedKeyUsages.Value
+    OR
+CA - CN from Issuer
+#>
+function New-CryptoProKeyContainerName {
+    param (
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true)]
+        # Certificate for input
+        [System.Security.Cryptography.X509Certificates.X509Certificate2]
+        $Certificate
+    )
+    $certDateValidRaw = $Certificate.NotAfter
+    [string]$certDateValidString = Get-Date -Date $certDateValidRaw -Format 'yyyy-MM-dd'
+
+    $certSubjectRaw = Filter-CertificateSubject -Subject $Certificate.Subject
+    $certIssuerRaw = Filter-CertificateSubject -Issuer $Certificate.Issuer
+    $certSubject = ConvertTo-PSObjectFromCertificateSubject -Subject $certSubjectRaw
+    $certIssuer = ConvertTo-PSObjectFromCertificateSubject -Subject $certIssuerRaw
+    $certSubjectOrgRaw = $certSubject.O
+    $certIssuerOrgRaw = $certIssuer.CN
+
+    if ($certSubjectOrgRaw -match '"') {
+        $subjArrTmp = $certSubjectOrgRaw | ConvertFrom-QuotedStringToArray
+        $oPFPrefix = New-Abbreviation -StringArray ($subjArrTmp[0].Split(' -')) -MaxWordLength 1
+        $orgName = New-Abbreviation -StringArray ($subjArrTmp[1].Split(' -'))
+        $orgNameFull = $oPFPrefix, $orgName -join '_'
+    } else {
+        $orgNameFull = New-Abbreviation -StringArray ($certSubjectOrgRaw.Split(' -')) -JoinChar '_'
+    }
+    $orgNameFull = $orgNameFull -replace '[^\w\s-]', '-'
+
+    $certSubjectPerson = $certSubject.SN, ($certSubject.G -replace ' ', '_') -join '_'
+    $certIssuerAbbrArr = ($certIssuerOrgRaw -replace '[^\w\s]', '') -split ' '
+    
+    $certIssuerAbbr = New-Abbreviation -StringArray $certIssuerAbbrArr -JoinChar '_'
+
+    $outString = $certDateValidString, $orgNameFull, $certSubjectPerson, $certIssuerAbbr -join '+'
+    return $outString
+}
+Export-ModuleMember -Function 'New-CryptoProKeyContainerName'

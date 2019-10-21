425f1c7223a0a27c5e35d08ec705494c4209c0d4
drlsdee <tracert0@gmail.com>
2019-10-21 20:47:06 +0500

    Filter-CertificateSubject.psm1
    Module for parsing 'Subject' and 'Issuer' fields.
    Now the module fields of one or multiple certificates.
    Accepts input as a [string[]] or as a [certificate[]].
    Outputs a [PSObject[]] with or without duplicates or empty attributes.
    Minimum required host version is still 3.0.

--- Functions/Filter-CertificateSubject.psm1
+++ Functions/Filter-CertificateSubject.psm1
@@ -1,4 +1,115 @@
+function Join-AttributeParts {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        # String array - splitted certificate field
+        [string[]]
+        $StringArray,
+
+        [Parameter()]
+        # Current string
+        [string]
+        $CurrentString    
+    )
+    # Attribute separator, default is '='
+    [char]$attributeSeparator = '='
+    [string]$myName = "[$($MyInvocation.MyCommand.Name)]:"
+    Write-Verbose -Message "$myName Starting function..."
+    if (-not $CurrentString) {
+        $CurrentString = $StringArray[0]
+    }
+    $indexCurrent = $StringArray.IndexOf($CurrentString)
+    Write-Verbose -Message "$myName Current string is [$indexCurrent]: `"$CurrentString`""
+    if ($CurrentString -notmatch $attributeSeparator) {
+        Write-Verbose -Message "$myName Current string [$indexCurrent]: `"$CurrentString`" - probably contains only part of attribute value. Nothing to return!"
+        return
+    } elseif ($indexCurrent -eq ($StringArray.Count - 1)) {
+        Write-Verbose -Message "$myName Current string [$indexCurrent]: `"$CurrentString`" - is the LAST string and contains attribute name. Returning current string."
+    } else {
+        $indexNext = $indexCurrent + 1
+        do {
+            $CurrentString = (@($CurrentString, $stringNext) | Where-Object {$_}) -join ', '
+            $stringNext = $StringArray[$indexNext]
+            $indexNext++
+            Write-Verbose -Message "$myName Next string [$indexNext]: `"$stringNext`"."
+            Write-Verbose -Message "$myName Current value of current string is: `"$CurrentString`"."
+        } while (($stringNext -notmatch $attributeSeparator) -and ($indexNext -lt $StringArray.Count))
+        Write-Verbose -Message "$myName Final string: `"$CurrentString`". Returning..."
+    }
+    Write-Verbose -Message "$myName End of function."
+    return $CurrentString
+}
+
+function Split-CertificateSubject {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        # String array from certificate 'Subject' or 'issuer' fields - already filtered and joined
+        [string[]]
+        $InputArray,
+
+        [Parameter()]
+        [ValidateSet('Upper','Lower','Preserve')]
+        # Switch case (default is UPPER)
+        [string]
+        $SwitchCase = 'Upper',
+
+        [Parameter()]
+        # Remove quotes
+        [switch]
+        $UnQuote,
+
+        [Parameter()]
+        # Select only unique attribute values
+        [switch]
+        $Unique,
+
+        [Parameter()]
+        # Skip empty attributes
+        [switch]
+        $SkipEmpty
+    )
+    # Field separator, default is '='
+    [char]$attributeSeparator = '='
+    [string]$myName = "[$($MyInvocation.MyCommand.Name)]:"
+    Write-Verbose -Message "$myName Starting function..."
+    $outputObject = New-Object -TypeName psobject
+    foreach ($certAttribute in $InputArray) {
+        switch ($SwitchCase) {
+            'Upper' {$certAttribute = $certAttribute.ToUpper()}
+            'Lower' {$certAttribute = $certAttribute.ToLower()}
+            Default {$certAttribute = $certAttribute}
+        }
+        [string[]]$certAttributeSplitted = $certAttribute.Split($attributeSeparator)
+        # Assuming that each attribute contains at least Attribute name
+        [string]$attributeName = $certAttributeSplitted[0]
+        [string[]]$attributeValue = $certAttributeSplitted[1]
+        if ($UnQuote) {
+            #$fieldValue = $fieldValue -replace '^"(.*)"$','$1'
+            Write-Verbose -Message "$myName Unquoting attribute value: `"$attributeValue`""
+            $attributeValue = $attributeValue -replace '"', ''
+        }
+        if ($SkipEmpty -and (-not $attributeValue)) {
+            Write-Warning -Message "$myName Value of attribute `"$attributeName`" is empty! SKIPPING..."
+        } else {
+            if (-not $attributeValue) {Write-Warning -Message "$myName Value of attribute `"$attributeName`" is empty! SKIPPING..."}
+            if (-not ($outputObject | Get-Member -Name $attributeName)) {
+                Write-Verbose -Message "$myName Adding field `"$attributeName`" to output..."
+                $outputObject | Add-Member -MemberType NoteProperty -Name $attributeName -Value $attributeValue
+            } elseif ($Unique -and ($outputObject.$attributeName -contains $attributeValue)) {
+                Write-Warning -Message "$myName Field `"$attributeName`" already present and contains value `"$attributeValue`"! Skipping..."
+            } else {
+                Write-Verbose -Message "$myName Field `"$attributeName`" already present! Adding value `"$attributeValue`""
+                $outputObject.$attributeName += $attributeValue
+            }
+        }
+    }
+    Write-Verbose -Message "$myName End of function."
+    return $outputObject
+}
+
 function Filter-CertificateSubject {
+    [CmdletBinding()]
     <#
     TODO: Write help and comments
     Host version:
@@ -7,131 +118,93 @@ function Filter-CertificateSubject {
         CurrentUICulture : ru-RU
     #>
     param (
-        # Certificate subject (string from certificate field)
+        # Certificate subject or issuer (as string from certificate fields)
         [Parameter(Mandatory=$true,
         ValueFromPipeline=$true,
-        #ValueFromPipelineByPropertyName=$true,
-        ParameterSetName = "Subject")]
-        [string]
+        ParameterSetName='Certificate')]
+        [System.Security.Cryptography.X509Certificates.X509Certificate2[]]
+        $Certificate,
+
+        # Certificate subject or issuer (as string from certificate field)
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true,
+        ParameterSetName='String')]
+        [string[]]
         $Subject,
 
-        # Certificate issuer (string from certificate field)
-        [Parameter(Mandatory=$true,
-        ValueFromPipeline=$true,
-        #ValueFromPipelineByPropertyName=$true,
-        ParameterSetName = "Issuer")]
+        [Parameter(ParameterSetName='Certificate')]
+        # Field of certificate
+        [ValidateSet('Subject','Issuer')]
         [string]
-        $Issuer,
+        $Field = 'Subject',
 
         [Parameter()]
+        [ValidateSet('Upper','Lower','Preserve')]
+        # Switch case (default is UPPER)
+        [string]
+        $SwitchCase = 'Upper',
+
+        [Parameter()]
+        # Remove quotes
         [switch]
-        $AsObject,
+        $UnQuote,
 
         [Parameter()]
-        [string]
-        $Separator = ", "
+        # Select only unique attribute values
+        [switch]
+        $UniqueAttribute,
+
+        [Parameter()]
+        # Select only unique subjects
+        [switch]
+        $UniqueSubject,
+
+        [Parameter()]
+        # Skip empty attributes
+        [switch]
+        $SkipEmpty,
+
+        [Parameter()]
+        # For multiple subjects; if set, merges all attributes with same names from all certificates into one field. "Unique" switch sets to "$true".
+        [switch]
+        $Merge
     )
-    begin {
-        if ($Subject) {
-            $InputString = $Subject
-        } elseif ($Issuer) {
-            $InputString = $Issuer
+    [string]$myName = "[$($MyInvocation.MyCommand.Name)]:"
+    Write-Verbose -Message "$myName Starting function..."
+    [string]$Separator = ", "
+    if ((-not $Subject) -and (-not $Certificate)) {
+        Write-Error -Message "$myName Subject or issuer not specified!"
+        return
+    } elseif ($Subject) {
+        $InputStringArray = $Subject
+    } elseif ($Certificate) {
+        Write-Verbose -Message "$myName Working on certificate with thumbprint `"$($Certificate.Thumbprint)`", field is `"$Field`""
+        $InputStringArray = $Certificate.$Field
+    }
+    if ($Merge) {
+        $UniqueAttribute = $true
+        $InputStringArray = $InputStringArray -join $Separator
+    }
+
+    [psobject[]]$outputObject = @()
+    foreach ($InputString in $InputStringArray) {
+        Write-Verbose -Message "$myName Start splitting string `"$InputString`"..."
+        $inputStringSplitted = $InputString -split $Separator
+        [string[]]$inputArray = $inputStringSplitted | ForEach-Object {Join-AttributeParts -StringArray $inputStringSplitted -CurrentString $_}
+        if (-not $inputArray) {
+            Write-Warning -Message "$myName Subject not parsed! Exiting..."
         } else {
-            Write-Error -Message "Neither subject nor issuer specified!"
-            break
-        }
-        Write-Verbose -Message "Start working with subject:"
-        Write-Verbose -Message $InputString
-        Write-Verbose -Message "Split subject by given separator:"
-        Write-Verbose -Message $Separator
-        $subjSplit = $InputString -split $Separator
-        foreach ($substr in $subjSplit) {
-            Write-Verbose -Message $substr
-        }
-        $arrayOfStrings = @()
-    }
-    process {
-        $Count = $subjSplit.Count
-        Write-Verbose  -Message "Subject has $Count unfiltered strings"
-        $Index = 0
-        while ($Index -lt $Count) {
-            Write-Verbose -Message "Index of current unfiltered string is: $Index"
-            $currentString = $subjSplit[$Index]
-            Write-Verbose -Message "String content is:"
-            Write-Verbose -Message $currentString
-            if ($currentString -match "=") {
-                Write-Verbose -Message "Current string starts with attribute name:"
-                Write-Verbose -Message ($currentString -replace '=[\w*\W*\s*]*')
-                $nextInd = $Index + 1
-                $nextString = $subjSplit[$nextInd]
-                if (-not $nextString) {
-                    Write-Verbose -Message "Current string is last string"
-                    $arrayOfStrings += $currentString
-                }
-                elseif ($nextString -match "=") {
-                    Write-Verbose -Message "The NEXT string starts with attribute name:"
-                    Write-Verbose -Message ($nextString -replace '=[\w*\W*\s*]*')
-                    $arrayOfStrings += $currentString
-                } else {
-                    Write-Verbose -Message "The NEXT string contains part of the attribute value"
-                    $ind = $nextInd
-                    do {
-                    $ind++
-                    Write-Verbose $ind
-                    $nxtStr = $subjSplit[$ind]
-                    Write-Verbose -Message $nxtStr
-                    } until ($nxtStr -match "=")
-                    $joinPart = $subjSplit[$nextInd..($ind - 1)] -join $Separator
-                    Write-Verbose -Message $joinPart
-                    $joinedString = $currentString,$joinPart -join $Separator
-                    Write-Verbose -Message $joinedString
-                    $arrayOfStrings += $joinedString
-                }
-            } else {
-                Write-Verbose -Message "The current string does not contain the attribute name"
-            }
-            $Index++
+            $outputObject += Split-CertificateSubject -InputArray $inputArray -UnQuote:$UnQuote -Unique:$UniqueAttribute -SkipEmpty:$SkipEmpty -SwitchCase $SwitchCase
         }
     }
-    end {
-        if ($AsObject) {
-            $arrayOfObjects = @()
-            foreach ($string in $arrayOfStrings) {
-                $strSplitted = $string -split "="
-                $attrName = $strSplitted[0]
-                Write-Verbose "Attribute name is $attrName"
-                $attrValue = $strSplitted[1]
-                Write-Verbose "Attribute value is $attrValue"
-                $strObj = @{
-                    "$attrName" = $attrValue
-                }
-                $arrayOfObjects += $strObj
-            }
-            $Out = $arrayOfObjects
-        } else {
-            $Out = $arrayOfStrings
-        }
-        return $Out
+    if ($UniqueSubject) {
+        Write-Verbose -Message "$myName Filtering unique subjects..."
+        $outputObject = $outputObject | Select-Object -Property * -Unique
     }
+    Write-Verbose -Message "$myName End of function."
+    return $outputObject
 }
 Export-ModuleMember -Function "Filter-CertificateSubject"
-
-function ConvertFrom-QuotedStringToArray {
-    param (
-        [Parameter(Mandatory=$true,
-        ValueFromPipeline=$true)]
-        # Input string with quotes
-        [string]
-        $InputString,
-
-        [Parameter()]
-        # Quote char
-        [char]
-        $QuoteChar = '"'
-    )
-    [array]$unQuotedArray = $InputString -split $QuoteChar | Where-Object -FilterScript {$_.Length -gt 0} | ForEach-Object {$_.Trim(' ')}
-    return $unQuotedArray
-}
 
 function New-Abbreviation {
     param (
@@ -163,26 +236,6 @@ function New-Abbreviation {
     return $outString
 }
 
-function ConvertTo-PSObjectFromCertificateSubject {
-    param (
-        [Parameter(Mandatory=$true)]
-        # Subject or issuer as array of strings, each contains both field name and value, i.e. "CN=Common Subject Name"
-        [string[]]
-        $Subject,
-
-        [Parameter()]
-        # Field separator, default is '='
-        [char]
-        $FieldSeparator = '='
-    )
-    $OutputObject = New-Object -TypeName psobject
-    foreach ($string in $Subject) {
-        $stringSeparated = $string -split $FieldSeparator
-        $OutputObject | Add-Member -MemberType NoteProperty -Name $stringSeparated[0] -Value $stringSeparated[1]
-    }
-    return $OutputObject
-}
-
 <#
 Date in format 'yyyy-MM-dd'
 OrgSubject - 'O' as abbreviation
@@ -191,40 +244,3 @@ Roles from OIDs and .Extensions.EnhancedKeyUsages.Value
     OR
 CA - CN from Issuer
 #>
-function New-CryptoProKeyContainerName {
-    param (
-        [Parameter(Mandatory=$true,
-        ValueFromPipeline=$true)]
-        # Certificate for input
-        [System.Security.Cryptography.X509Certificates.X509Certificate2]
-        $Certificate
-    )
-    $certDateValidRaw = $Certificate.NotAfter
-    [string]$certDateValidString = Get-Date -Date $certDateValidRaw -Format 'yyyy-MM-dd'
-
-    $certSubjectRaw = Filter-CertificateSubject -Subject $Certificate.Subject
-    $certIssuerRaw = Filter-CertificateSubject -Issuer $Certificate.Issuer
-    $certSubject = ConvertTo-PSObjectFromCertificateSubject -Subject $certSubjectRaw
-    $certIssuer = ConvertTo-PSObjectFromCertificateSubject -Subject $certIssuerRaw
-    $certSubjectOrgRaw = $certSubject.O
-    $certIssuerOrgRaw = $certIssuer.CN
-
-    if ($certSubjectOrgRaw -match '"') {
-        $subjArrTmp = $certSubjectOrgRaw | ConvertFrom-QuotedStringToArray
-        $oPFPrefix = New-Abbreviation -StringArray ($subjArrTmp[0].Split(' -')) -MaxWordLength 1
-        $orgName = New-Abbreviation -StringArray ($subjArrTmp[1].Split(' -'))
-        $orgNameFull = $oPFPrefix, $orgName -join '_'
-    } else {
-        $orgNameFull = New-Abbreviation -StringArray ($certSubjectOrgRaw.Split(' -')) -JoinChar '_'
-    }
-    $orgNameFull = $orgNameFull -replace '[^\w\s-]', '-'
-
-    $certSubjectPerson = $certSubject.SN, ($certSubject.G -replace ' ', '_') -join '_'
-    $certIssuerAbbrArr = ($certIssuerOrgRaw -replace '[^\w\s]', '') -split ' '
-    
-    $certIssuerAbbr = New-Abbreviation -StringArray $certIssuerAbbrArr -JoinChar '_'
-
-    $outString = $certDateValidString, $orgNameFull, $certSubjectPerson, $certIssuerAbbr -join '+'
-    return $outString
-}
-Export-ModuleMember -Function 'New-CryptoProKeyContainerName'
